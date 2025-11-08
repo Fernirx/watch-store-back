46d609f69ea7c67a1121c0dbcd7fcc8b11dbb491
Fernirx <anhtuhungdeveloper@gmail.com>
2025-11-08 12:00:15 +0700

    Rename application classes from TawatchBackendApplication to TawatchApplication

--- tawatch-starter/src/main/java/vn/fernirx/tawatch/starter/TawatchApplication.java
+++ tawatch-starter/src/main/java/vn/fernirx/tawatch/starter/TawatchApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TawatchBackendApplication {
+public class TawatchApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TawatchBackendApplication.class, args);
+        SpringApplication.run(TawatchApplication.class, args);
     }
 
 }
